e0b31c0eceab91a0be3eabdc762b0eb00ac903d6
the-rk <95013057+the-rk@users.noreply.github.com>
2022-06-09 15:50:18 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+function setup() {
+  canvas = createCanvas(300, 300);
+  canvas.center();
+  video = createCapture(VIDEO);
+  video.hide();
+  classifier = ml5.imageClassifier('MobileNet',modelLoaded);
+}
+function modelLoaded(){
+  console.log("Model is loaded");
+}
+function draw(){
+  image(video,0,0,300,300);
+  classifier.classify(video, gotResult);
+}
+previous_result = "";
+function gotResult(error, results){
+  if(error){
+    console.error(error);
+  }
+  else{
+    if((results[0].confidence>0.5) && (previous_result != results[0].label)){
+      console.log(resuts);
+      previous_result = results[0].label;
+      synth = window.speechSynthesis;
+      speak_data = "Object detected is - " + results[0].label;
+    utterThis = new SpeechSynthesisUtterance(speak_data);
+  synth.speak(utterThis);
+  document.getElementById("result_object").innerHTML = results[0].label;
+  document.getElementById("result_accuracy").innerHTML = results[0].confidence.toFixed(2);
+}
+  }
+}
+
+
